eba9d25de19bfcb8df77cf36c1210c19745fc462
Daria Ananchenko <ananchenko.daria@mail.ru>
2022-11-30 20:55:19 +0500

    Реализовано API для определения ключевых фраз в научных текстах

---
+++ extraction.py
@@ -0,0 +1,23 @@
+from transformers import (
+    TokenClassificationPipeline,
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+)
+from transformers.pipelines import AggregationStrategy
+import numpy as np
+
+class KeyphraseExtractionPipeline(TokenClassificationPipeline):
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(
+            model=AutoModelForTokenClassification.from_pretrained(model),
+            tokenizer=AutoTokenizer.from_pretrained(model),
+            *args,
+            **kwargs
+        )
+
+    def postprocess(self, model_outputs):
+        results = super().postprocess(
+            model_outputs=model_outputs,
+            aggregation_strategy=AggregationStrategy.SIMPLE,
+        )
+        return np.unique([result.get("word").strip() for result in results])
---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from extraction import KeyphraseExtractionPipeline
+
+
+class Item(BaseModel):
+    text:str
+
+    
+app = FastAPI()
+model_name = "ml6team/keyphrase-extraction-kbir-inspec"
+extractor = KeyphraseExtractionPipeline(model=model_name)
+
+
+@app.get("/")
+def root():
+    html_page = """
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Учебный проект</title>
+    </head>
+    <body>
+        <main>
+            <h1>Это ИИ приложение для курса Программная инженерия</h1>  
+        </main>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_page, status_code=200)
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    keyphrases = list(extractor(item.text))
+    return {"keyphrases": keyphrases}
